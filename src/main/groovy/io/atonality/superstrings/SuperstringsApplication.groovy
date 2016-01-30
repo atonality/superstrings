e05a9398343bd6a34e6a291d0cf103a1a4987ad3
John Hotovy <john.hotovy@gmail.com>
2016-01-29 20:39:01 -0600

    Create project shell

---
+++ src/main/groovy/io/atonality/superstrings/SuperstringsApplication.groovy
@@ -0,0 +1,11 @@
+package io.atonality.superstrings
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class SuperstringsApplication {
+
+    static void main(String[] args) {
+        println("Superstrings")
+    }
+}
