13333ff7dc1f7c3e164347c82e298639b31cf83a
John Hotovy <john.hotovy@gmail.com>
2016-02-12 17:33:36 -0600

    Preserve escape sequences when translating java strings

--- src/main/groovy/io/atonality/superstrings/JavaSanitizer.groovy
+++ src/main/groovy/io/atonality/superstrings/JavaSanitizer.groovy
@@ -11,19 +11,29 @@ class JavaSanitizer extends Sanitizer {
     // %[argument_index$][flags][width][.precision][t]conversion
     static final String FMT_SPECIFIER_REGEX = '%(\\d+\\$)?([-#+ 0,(\\<]*)?(\\d+)?(\\.\\d+)?([tT])?([a-zA-Z%])'
 
+    // http://stackoverflow.com/questions/1367322/what-are-all-the-escape-characters-in-java
+    // TODO: add additional escape sequences
+    static final String ESCAPE_SEQUENCE_REGEX = "(\\\\[btnfr])+"
+
     Pattern fmtSpecifierPattern
+    Pattern escapeSequencePattern
 
     JavaSanitizer(SuperstringsMetadata metadata) {
         super(metadata)
         fmtSpecifierPattern = Pattern.compile(FMT_SPECIFIER_REGEX)
+        escapeSequencePattern = Pattern.compile(ESCAPE_SEQUENCE_REGEX)
     }
 
     @Override
     List<String> getPositionalArguments(StringResource resource) {
         def args = super.getPositionalArguments(resource)
-        def fmtSpecifiers = resource.value.findAll(fmtSpecifierPattern)
 
+        def fmtSpecifiers = resource.value.findAll(fmtSpecifierPattern)
         args += fmtSpecifiers
+
+        def escapeSequences = resource.value.findAll(escapeSequencePattern)
+        args += escapeSequences
+
         return args
     }
 }
